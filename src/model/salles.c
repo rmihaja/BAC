436c6878195802428ec81881ef452070c446eccd
Rmihaja <50056520+Rmihaja@users.noreply.github.com>
2022-04-23 12:19:48 +0200

    🚀 Add source code for collections module

---
+++ src/model/salles.c
@@ -0,0 +1,20 @@
+// ? structure modèle de toutes les modules du projet
+
+#ifdef DEBUG
+#include "tests.h"
+#endif
+
+// attributs, constructor, methods here
+
+#ifdef TEST
+
+int main() {
+
+    // init
+
+    // testing
+
+    return 0;
+}
+
+#endif
