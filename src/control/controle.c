94b47a40f8a296691f7ed65fbbcce2c2fbbf3afc
valembrouille <90392480+valembrouille@users.noreply.github.com>
2022-04-28 00:13:56 +0200

    formation part 1

--- src/control/controle.c
+++ src/control/controle.c
@@ -1,10 +1,28 @@
 // ? structure modèle de toutes les modules du projet
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "enseignants.h"
+#include "salles.h"
+#include "formation.h"
+
 
 #ifdef DEBUG
 #include "tests.h"
 #endif
 
-// attributs, constructor, methods here
+struct s_controle{
+    bool isAdmin;
+    Enseignants enseignants;
+    Salles salles;
+    Formation formations;
+}*Controle;
+
+Controle controle(){
+    
+}
+
+
 
 #ifdef TEST
 
--- src/model/formation.c
+++ src/model/formation.c
@@ -1,43 +1,65 @@
-#include "collections/creneau.h"
-#include "types/formation.h"
+#include "formation.h"
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct s_edt{
+    Creneau creneau;
+    Creneau* suivant;
+}*Edt;
 
 struct s_formation {
     char * nom;
-    Creneau creneau[4];
+    edt edt;
+    int nbr;
 };
+
 
 Formation formation(char* n){
     Formation f=(Formation)malloc(sizeof(struct s_formation));
+    Edt edt=malloc(sizeof(sizeof(struct s_edt)));
+    f->edt->suivant=edt;
     f->nom=n;
-    for (int i=0; i<4 ;i++){
-        f->creneau[i]="NULL";
-    }
+    f->nbr=0;
     return f;
 }
 
+Edt edt(Formation f, Creneau c){
+    Edt e=(Edt)malloc(sizeof(struct s_edt));
+    e->creneau=c;
+    e->suivant=f->edt;
+    edt->suivant=e;
+}
+
 bool estComplet(Formation f){
-    bool b=true;
-    for(int i=0;i<4;i++){
-        if(f->creneau[i]=="NULL" && b){
-            b=false;
-        }
+    if(nbr==4){
+        return true;
+    }else{
+        return false;
     }
-    return b;
 }
 
 Formation ajouterC(Formation f, Creneau c){
     assert(!estComplet(f));
-    for(int i=0; i<4; i++){
-        
+    Edt e=edt(c);
+    if(f->nbr==0){
+        f->edt=e;
+        f->nbr=1;
+    }else{
+        e->suivant=f->edt;
+        f->edt=e;
+        f->nbr++;
     }
+    return f;
 }
 
 Formation supprimerH (Formation f, Horaire h){
-    for(int i=0; i<4; i++){
-        if(f->creneau[i]->horaire==h){
-            f->creneau[i]="NULL";
-        }
+    assert(f->nbr!=0);
+    Edt precedent=f->edt;
+    Edt courant=f->edt->suivant;
+    int i=0;
+    while(i<f->nbr && ){
+
     }
     return f;
 }
